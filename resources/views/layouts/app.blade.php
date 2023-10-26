89a46be0ce36def10c9cb4f5c0ca65dee0b362fb
Marco Zellini <zellinimarco@gmail.com>
2023-10-26 16:12:29 +0200

    Initial Commit + Add Header

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="../../img/favicon.ico" />
+    <title>DC Comics - @yield('page-title', 'Welcome')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    @include('partials.header')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header id="app_header">
+    <div class="header_top">
+        <div class="container d-flex justify-content-end py-1">
+            <span class="mx-3">DC POWER<small>sm</small> VISA<i class="fa-regular fa-registered fa-xs"></i></span>
+            <span class="mx-3">Additional DC sites <i class="fa-solid fa-sort-down"></i></span>
+        </div>
+    </div>
+
+    <div class="container">
+        <nav class="navbar navbar-expand justify-content-between p-0">
+            <a class="navbar-brand" href="#">
+                <img class="img-fluid" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC_logo">
+            </a>
+
+            <div class="nav_body d-flex align-items-center">
+                <div class="nav navbar-nav gx-3">
+                    <a class="nav-item nav-link">characters</a>
+                    <a class="nav-item nav-link" href="{{route('comics')}}">comics</a>
+                    <a class="nav-item nav-link">movies</a>
+                    <a class="nav-item nav-link">tv</a>
+                    <a class="nav-item nav-link">games</a>
+                    <a class="nav-item nav-link">collectibles</a>
+                    <a class="nav-item nav-link">videos</a>
+                    <a class="nav-item nav-link">fans</a>
+                    <a class="nav-item nav-link">news</a>
+                    <a class="nav-item nav-link">shop</a>
+
+                </div>
+                <form action="" class="ms-3">
+                    <input type="search" placeholder="SEARCH" class="text-end">
+                </form>
+            </div>
+
+
+        </nav>
+    </div>
+</header>
