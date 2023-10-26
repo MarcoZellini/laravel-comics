8dcad407331568fe4ee10932f4b5bd1f772466d9
Marco Zellini <zellinimarco@gmail.com>
2023-10-26 16:36:44 +0200

    Add Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,109 @@
+<footer id="app_footer">
+
+    <section id="footer_top">
+        <div class="container py-5">
+            <div class="row align-items-center">
+                <div class="col-7">
+                    <div id="useful_links" class="row text-light">
+
+                        <div class="row row-cols-1 row-cols-md-3">
+
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <h5 class="text-uppercase">DC Comics</h5>
+                                    <li><a href="" class="text-decoration-none text-light">Lorem.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Error?</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Maiores.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Doloribus.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                </ul>
+                                <!-- /.list-unstyled -->
+                                <ul class="list-unstyled">
+                                    <h5 class="text-uppercase">Shop</h5>
+                                    <li><a href="" class="text-decoration-none text-light">Lorem.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Eos.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Inventore!</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Expedita!</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                </ul>
+                                <!-- /.list-unstyled -->
+                            </div>
+
+
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <h5 class="text-uppercase">DC</h5>
+                                    <li><a href="" class="text-decoration-none text-light">Lorem.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Ipsam?</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Beatae?</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Modi?</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                </ul>
+                                <!-- /.list-unstyled -->
+                            </div>
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <h5 class="text-uppercase">Sites</h5>
+                                    <li><a href="" class="text-decoration-none text-light">Lorem.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Repudiandae?</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Ratione.</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                    <li><a href="" class="text-decoration-none text-light">Nam!</a>
+                                        <!-- /.text-decoration-none text-light --></li>
+                                </ul>
+                                <!-- /.list-unstyled -->
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- /.col-7 -->
+                <div class="col-5">
+                    <div class="position-relative d-flex justify-content-center align-items-center">
+                        <img class="img-fluid position-absolute" src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="">
+                    </div>
+                </div>
+                <!-- /.col-5 -->
+            </div>
+        </div>
+    </section>
+
+    <section id="footer_bottom" class="py-5">
+        <div class="container d-flex justify-content-between align-items-center">
+            <!-- Bottone signup -->
+            <a class="btn btn-outline-primary rounded-0 text-light text-uppercase d-flex align-items-center py-2">sign-up now!</a>
+
+            <!-- Lista di social -->
+            <ul class="social d-flex align-items-center m-0">
+                <a class="nav-item nav-link mx-1" href="#">Follow us</a>
+                <a class="nav-item nav-link mx-1" href="#">
+                    <img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="">
+                </a>
+                <a class="nav-item nav-link mx-1" href="#">
+                    <img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="">
+                </a>
+                <a class="nav-item nav-link mx-1" href="#">
+                    <img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="">
+                </a>
+                <a class="nav-item nav-link mx-1" href="#">
+                    <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="">
+                </a>
+                <a class="nav-item nav-link mx-1" href="#">
+                    <img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="">
+                </a>
+            </ul>
+        </div>
+    </section>
+
+</footer>
